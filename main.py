53907c5ab5a15d6544243b7504098e426b20e5e0
jinyanhuan <jinyanhuan@gmail.com>
2019-08-23 17:05:49 +0800

    fix some problems

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import r2_score
+import matplotlib.pyplot as plt
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.externals import joblib
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
-import matplotlib.pyplot as plt
+
 import load_data
 import process_data
 
@@ -59,11 +60,13 @@ def main():
     for i in range(10):
         train_x, test_x, train_y, test_y = train_test_split(X, y, test_size=0.3)
         print("\n【第%s次测试】" % (i + 1))
+		
         model = RandomForestRegressor(n_estimators=100).fit(train_x, train_y)
         predict = model.predict(test_x)
         score_predict = r2_score(test_y, predict)
         importance = model.feature_importances_
         print("R^2 score value: %s" % score_predict)
+		
         score_list.append(score_predict)
         model_list.append(model)
         importance_list.append(importance)
@@ -76,10 +79,11 @@ def main():
         sum_score += float(i)
     avg_score = sum_score / len(score_list)
     best_score = max(score_list)
+	
     print("平均测试分数为:%s" % avg_score)
     print("最高测试分数为:%s" % best_score)
-    # print(score_list.index(max(score_list)))
     print("最好的模型是:%s" % best_model)
+	
     best_importance = importance_list[score_list.index(best_score)]
 
     # draw feature importance
